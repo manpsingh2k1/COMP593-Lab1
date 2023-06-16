0d60d0111953b1847ee33a1fbd6e0577a3e684a8
manpsingh2k1 <136854066+manpsingh2k1@users.noreply.github.com>
2023-06-16 17:15:24 -0400

    Modified both files in the repo clone
    - Deleted a Print() statement from script1.py
    - Added two Print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Sparse is better than dense.")
-print("Unless explicitly silenced.")
--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 # Print lines from the Zen of Python
 print("Now is better than never")
 print("If the implementation is hard to explain, it's a bad idea.")
+print("Now is better than never")
+print("If the implementation is hard to explain, it's a bad idea.")
